5d0d0fa1cc4e6318ec17784ccfb9847d3538b0b8
natya2203 <nastya_2203@ukr.net>
2020-02-04 02:33:28 +0200

    Updates

---
+++ precache-manifest.ec78bb1988980bac8752306be014bbfa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1987cf408c5e5a783becdf44f12731ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68fa1d0de863c6fa1641",
+    "url": "/static/css/main.0a85089c.chunk.css"
+  },
+  {
+    "revision": "f980b380ef9bea927f96",
+    "url": "/static/js/2.0abdc706.chunk.js"
+  },
+  {
+    "revision": "6dbd9e7d4b65b27382d6cb0964c77a31",
+    "url": "/static/js/2.0abdc706.chunk.js.LICENSE"
+  },
+  {
+    "revision": "68fa1d0de863c6fa1641",
+    "url": "/static/js/main.591eb990.chunk.js"
+  },
+  {
+    "revision": "c456da327278e47f5781",
+    "url": "/static/js/runtime-main.072b0f90.js"
+  },
+  {
+    "revision": "91bd4de7f820d26a905512de0d4fe133",
+    "url": "/static/media/Spinner-1s-200px.91bd4de7.svg"
+  },
+  {
+    "revision": "2abfb48b3d7a7b5e256b12f35ebcfbd8",
+    "url": "/static/media/footer.2abfb48b.svg"
+  },
+  {
+    "revision": "5ad4668f37a4dfb1c969fc69b77c113d",
+    "url": "/static/media/icon.5ad4668f.svg"
+  },
+  {
+    "revision": "0676b50eaa8c663bf81853cf2bdc9e4b",
+    "url": "/static/media/iphon.0676b50e.svg"
+  },
+  {
+    "revision": "5e326211a5119b0f84d51394f064b4d6",
+    "url": "/static/media/mackbook.5e326211.svg"
+  }
+]);
